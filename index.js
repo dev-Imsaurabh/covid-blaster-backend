351369b8a026fc389fa6708c65b89a9d71dc2af8
dev-Iamsaurabh <sau4478@gmail.com>
2023-03-03 17:48:32 +0530

    added throtling

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express()
 const {Server} = require("socket.io")
 const cors = require("cors")
+const wildcard =require('socketio-wildcard')();
+
 app.use(cors())
 require("dotenv").config()
 
@@ -24,13 +26,25 @@ const io = new Server(server,{
         methods:["GET","POST"]
     }
 })
+io.use(wildcard);
+
 
 io.on("connection",(socket)=>{
+    const THROTTLE_INTERVAL = 1000;
 
     console.log("User connected :"+socket.id)
     socket.on("send_message",(data)=>{
+        const now = Date.now();
 
-        socket.broadcast.emit("receive_message",data)
+    if (now - lastEventTime > THROTTLE_INTERVAL) {
+      // process the event normally
+      lastEventTime = now;
+    } else {
+      // throttle the event
+      console.log(`Throttling event: ${packet.data}`);
+      socket.broadcast.emit("receive_message",data)
+    }
+
 
     })
 
